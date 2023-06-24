ba76e0ceb9c420e32c60689c602dc98c90b33e1d
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-06-24 13:38:40 +0300

    T03-P11

---
+++ JS Front-End - 2023/T03_JSSyntaxFundamentals/T03_JSSyntaxFundamentals_Lab/P11_SumFirstAndLastArrayElements.js
@@ -0,0 +1,8 @@
+function sumFirstAndLast(input) {
+    let first = input[0];
+    let last = input[input.length - 1];
+    let sum = first + last;
+    console.log(sum);
+}
+
+sumFirstAndLast([10, 17, 22, 33]);
