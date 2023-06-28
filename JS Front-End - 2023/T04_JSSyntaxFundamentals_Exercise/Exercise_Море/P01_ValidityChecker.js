a6d74c1f3e81cbfc0434cff38f29ffab1a0cd5f7
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-06-29 00:29:47 +0300

    Update P01_ValidityChecker.js

--- JS Front-End - 2023/T04_JSSyntaxFundamentals_Exercise/Exercise_Море/P01_ValidityChecker.js
+++ JS Front-End - 2023/T04_JSSyntaxFundamentals_Exercise/Exercise_Море/P01_ValidityChecker.js
@@ -1,18 +1,21 @@
-function calculateDistance(x1, y1, x2, y2) {
-    return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-}
+function validity(x1, y1, x2, y2) {
 
-function isDistanceValid(x1, y1, x2, y2) {
-    const distance = calculateDistance(x1, y1, 0, 0);
-    const isValid = Number.isInteger(distance);
-    const status = isValid ? 'valid' : 'invalid';
+    function calculateDistance(x1, y1, x2, y2) {
+        return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+    }
 
-    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is ${status}`);
-}
-function sovle(x1, y1, x2, y2) {
+    function isDistanceValid(x1, y1, x2, y2) {
+        const distance = calculateDistance(x1, y1, x2, y2);
+        const isValid = Number.isInteger(distance);
+        const status = isValid ? "valid" : "invalid";
+
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is ${status}`);
+    }
+
     isDistanceValid(x1, y1, 0, 0);
     isDistanceValid(x2, y2, 0, 0);
     isDistanceValid(x1, y1, x2, y2);
 }
 
-sovle(3, 0, 0, 4);
+validity(3, 0, 0, 4);
+validity(2, 1, 1, 1);
