3a6231f124b5b0ebfe3ce122dc5e4b0467f26145
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-07-06 20:06:32 +0300

    T07-P03

---
+++ JS Front-End - 2023/Т07_ObjectsАndClasses/P03_CityTaxes.js
@@ -0,0 +1,18 @@
+function cityTaxes(name, population, treasury) {
+    return {
+        name: name,
+        population: population,
+        treasury: treasury,
+        taxRate: 10,
+        collectTaxes() { this.treasury += this.population * this.taxRate; },
+        applyGrowth(percentage) { this.population += (percentage / 100) * this.population; },
+        applyRecession(percentage) { this.treasury -= (percentage / 100) * this.treasury; },
+    }
+}
+
+const city =
+    cityTaxes('Tortuga', 7000, 15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
