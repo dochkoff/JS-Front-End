e7defab7a18ea208620dcb9164fcd05b3d4fed43
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-07-11 00:10:16 +0300

    T08-P03

---
+++ JS Front-End - 2023/Т08_ObjectsАndClasses_Exercise/Exercise/P03_StoreProvision.js
@@ -0,0 +1,25 @@
+function solve(stocks, delivery) {
+    const products = [...stocks, ...delivery];
+
+    const stock = products.reduce((acc, curr,i) => {
+        if (i%2===0) {
+            if (!acc.hasOwnProperty(curr)) {
+                acc[curr] = Number(products[i + 1]);
+            } else {
+                acc[curr] += Number(products[i + 1]);
+            }
+        }
+
+        return acc;
+    }, {})
+
+    const keys=Object.keys(stock);
+
+    for (const key of keys) {
+        console.log(`${key} -> ${stock[key]}`);
+    }
+}
+
+solve(['Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'],
+    ['Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30']
+);
