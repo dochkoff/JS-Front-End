5d1ffec3c38be401dc5dd03ecbf8124bbda40863
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-07-11 21:34:50 +0300

    T08-P04

---
+++ JS Front-End - 2023/Т08_ObjectsАndClasses_Exercise/Exercise/P04_Movies.js
@@ -0,0 +1,86 @@
+function solve(input) {
+    class MovieLibrary {
+        constructor() {
+            this.movies = [];
+        }
+
+        addMovie(command) {
+            const [_, name] = command.split("addMovie ");
+            this.movies.push({ name });
+        }
+
+        addDirector(command) {
+            const [movieName, directorName] = command.split(" directedBy ");
+            const movie = this.movies.find((m) => m.name === movieName);
+
+            if (movie) {
+                movie.director = directorName;
+            }
+        }
+
+        addDate(command) {
+            const [movieName, date] = command.split(" onDate ");
+            const movie = this.movies.find((m) => m.name === movieName);
+
+            if (movie) {
+                movie.date = date;
+            }
+        }
+    }
+
+    function createMovieLibrary() {
+        let movies = [];
+
+        return {
+            addMovie: (command) => {
+                const [_, name] = command.split("addMovie ");
+                movies.push({ name });
+            },
+            addDirector: (command) => {
+                const [movieName, directorName] = command.split(" directedBy ");
+                const movie = movies.find((m) => m.name === movieName);
+
+                if (movie) {
+                    movie.director = directorName;
+                }
+            },
+            addDate: (command) => {
+                const [movieName, date] = command.split(" onDate ");
+                const movie = movies.find((m) => m.name === movieName);
+
+                if (movie) {
+                    movie.date = date;
+                }
+            },
+            getMovies() {
+                return movies;
+            },
+        };
+    }
+
+    const movieLibrary = new MovieLibrary();
+
+    input.forEach((command) => {
+        if (command.includes("addMovie")) {
+            movieLibrary.addMovie(command);
+        } else if (command.includes("directedBy")) {
+            movieLibrary.addDirector(command);
+        } else if (command.includes("onDate")) {
+            movieLibrary.addDate(command);
+        }
+    });
+
+    movieLibrary.movies
+        .filter((m) => m.name && m.director && m.date)
+        .forEach((m) => console.log(JSON.stringify(m)));
+}
+
+solve([
+    'addMovie Fast and Furious',
+    'addMovie Godfather',
+    'Inception directedBy Christopher Nolan',
+    'Godfather directedBy Francis Ford Coppola',
+    'Godfather onDate 29.07.2018',
+    'Fast and Furious onDate 30.07.2018', 'Batman onDate 01.08.2018',
+    'Fast and Furious directedBy Rob Cohen']);
+
