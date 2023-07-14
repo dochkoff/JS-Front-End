c2e15f83680dcae459759b799a324f1a1e196d07
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-07-14 23:46:43 +0300

    T09-P06

--- JS Front-End - 2023/Т09_DOMАndEvents/Т09_DOMАndEvents_Lab/05. Delete-From-Table/app.js
+++ JS Front-End - 2023/Т09_DOMАndEvents/Т09_DOMАndEvents_Lab/05. Delete-From-Table/app.js
@@ -1,3 +1,21 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let emails = document.querySelectorAll('#customers tr td:nth-child(2)');
+    let emailForDelition = document.getElementsByName('email')[0].value;
+
+    for (const email of emails) {
+        if (email.textContent===emailForDelition) {
+            let row = email.parentNode;
+            row.parentNode.removeChild(row);
+
+                document
+                .getElementById('result')
+                .textContent = "Deleted.";
+            
+            return;
+        }
+
+        document
+            .getElementById('result')
+            .textContent = "Not found.";
+    }
 }
--- JS Front-End - 2023/Т09_DOMАndEvents/Т09_DOMАndEvents_Lab/06. Add-Delete/app.js
+++ JS Front-End - 2023/Т09_DOMАndEvents/Т09_DOMАndEvents_Lab/06. Add-Delete/app.js
@@ -1,3 +1,24 @@
 function addItem() {
-    //TODO...
+    let newElement = document.getElementById('newItemText').value;
+    let list = document.getElementById('items');
+
+    if (newElement.length === 0) return;
+
+    let listItem=document.createElement('li');
+    listItem.textContent = newElement;
+
+    let remove = document.createElement('a');
+    let linkText=document.createTextNode('[Delete]');
+
+    remove.appendChild(linkText);
+    remove.href='#';
+    remove.addEventListener('click', deleteItem);
+
+    listItem.appendChild(remove);
+    list.appendChild(listItem);
+    document.getElementById('newItemText').value = '';
+
+    function deleteItem() {
+        listItem.remove(); 
+    }
 }
