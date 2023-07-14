66ed166c6d65ec250f7018cb7aa81b7a18542bd4
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-07-15 00:41:19 +0300

    T09-P08

--- JS Front-End - 2023/Т09_DOMАndEvents/Т09_DOMАndEvents_Lab/08. Sum-Table/SumTable.js
+++ JS Front-End - 2023/Т09_DOMАndEvents/Т09_DOMАndEvents_Lab/08. Sum-Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
+    let productPrices = Array.from(document.querySelectorAll('td:nth-child(2)'));
+    let sum = 0;
 
+    for (let price of productPrices) {
+        sum += Number(price.textContent);
+    }
+
+    document.getElementById('sum').textContent = sum;
 }
