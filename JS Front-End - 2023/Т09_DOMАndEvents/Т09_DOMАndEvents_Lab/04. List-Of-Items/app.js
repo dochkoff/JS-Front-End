7032bd5c60c200313b0f9e53ac23e1d6306e4cd9
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-07-14 22:27:22 +0300

    T09-P04

--- JS Front-End - 2023/Т09_DOMАndEvents/Т09_DOMАndEvents_Lab/04. List-Of-Items/app.js
+++ JS Front-End - 2023/Т09_DOMАndEvents/Т09_DOMАndEvents_Lab/04. List-Of-Items/app.js
@@ -1,3 +1,9 @@
 function addItem() {
-    console.log('TODO:...');
+    let allItems = document.getElementById('items');
+    let newItem = document.getElementById('newItemText').value;
+    let li=document.createElement('li');
+
+    li.appendChild(document.createTextNode(newItem));
+    allItems.appendChild(li);
+    document.getElementById('newItemText').value = '';
 }
