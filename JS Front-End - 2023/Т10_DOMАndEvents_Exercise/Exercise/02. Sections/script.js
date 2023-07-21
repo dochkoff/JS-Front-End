b0320c28caada14ea156ba0c7e462abe9441c757
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-07-21 19:17:25 +0300

    T10-P02

--- JS Front-End - 2023/Т10_DOMАndEvents_Exercise/Exercise/02. Sections/script.js
+++ JS Front-End - 2023/Т10_DOMАndEvents_Exercise/Exercise/02. Sections/script.js
@@ -1,3 +1,25 @@
 function create(words) {
-   console.log('TODO:...');
+   const container=document.querySelector("#content");
+
+   function onClick(e) {
+      const invisibleParagraph=e.currentTarget.querySelector("p");
+      invisibleParagraph.style.display = "block";
+   }
+   
+   function createDiv(word) {
+      const div = document.createElement("div");
+      const p = document.createElement("p");
+
+      p.textContent = word;
+      p.style.display = "none";
+      
+      div.appendChild(p);
+      div.addEventListener("click", onClick);
+
+      return div;
+   };
+   
+   words.forEach(word => {
+      container.appendChild(createDiv(word));
+   });
 }
