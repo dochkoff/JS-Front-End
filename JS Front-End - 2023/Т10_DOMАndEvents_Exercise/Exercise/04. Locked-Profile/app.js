fd6221bb185568a9bab6dfb891ce6b6de10ece99
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-07-21 23:15:16 +0300

    T10-P04

--- JS Front-End - 2023/Т10_DOMАndEvents_Exercise/Exercise/04. Locked-Profile/app.js
+++ JS Front-End - 2023/Т10_DOMАndEvents_Exercise/Exercise/04. Locked-Profile/app.js
@@ -1,3 +1,24 @@
 function lockedProfile() {
-    console.log('TODO...')
+    
+    const buttons = Array.from(document.querySelectorAll('button'));
+
+    buttons.forEach(button => {
+        button.addEventListener("click", handleClick);
+    });
+
+    function handleClick(e) {
+        const radioLocked = e.currentTarget.parentElement.querySelector(
+            'input[type="radio"]'
+        );
+
+        if (radioLocked.checked) {
+            return;
+        }
+
+        const info = e.currentTarget.parentElement.querySelector("div");
+        const isHidden = e.currentTarget.textContent === "Show more";
+        
+        info.style.display = isHidden ? "block" : "none";
+        e.currentTarget.textContent=isHidden?"Hide it":"Show more"
+    }
 }
