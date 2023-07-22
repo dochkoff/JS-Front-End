d8f4be585f1c894ec51a0076a3b2bb2eeae8fb33
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-07-22 12:47:45 +0300

    T10-P07

--- JS Front-End - 2023/Т10_DOMАndEvents_Exercise/Exercise/07. Format the Text/solution.js
+++ JS Front-End - 2023/Т10_DOMАndEvents_Exercise/Exercise/07. Format the Text/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO
+  const input = document.querySelector('#input').value.split('.');
+  const output = document.querySelector('#output');
+
+  //remove last empty element
+  input.pop();
+
+  while (input.length>0) {
+    const paragraph = document.createElement('p');
+    
+    paragraph.textContent = input
+      .splice(0, 3)
+      .map((text) => text.trim())
+      .join('. ') + '.';
+    
+    output.appendChild(paragraph);
+  }
 }
