86a27030252ad596d3d4e5a2763165ff3dc6eaa9
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-07-22 12:18:02 +0300

    T10-P06

--- JS Front-End - 2023/Т10_DOMАndEvents_Exercise/Exercise/06. Table - Search Engine/solution.js
+++ JS Front-End - 2023/Т10_DOMАndEvents_Exercise/Exercise/06. Table - Search Engine/solution.js
@@ -2,7 +2,18 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const searchedContent = document.querySelector('#searchField').value;
+      const tableData = Array.from(document.querySelectorAll('tbody td'));
+      const oldResult = Array.from(document.querySelectorAll('tbody tr.select'));
 
+      oldResult.forEach(row => {
+         row.classList.remove('select');
+      });
+
+      tableData.forEach(dataRow => {
+         if (dataRow.textContent.includes(searchedContent)) {
+            dataRow.parentElement.classList.add("select");
+         }
+      });
    }
 }
