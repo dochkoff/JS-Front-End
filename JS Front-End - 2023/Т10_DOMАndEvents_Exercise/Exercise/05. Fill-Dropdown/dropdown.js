bf21d69272654b0b5090bb5aa5ced1eec322a46e
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-07-22 11:41:14 +0300

    T10-P05

--- JS Front-End - 2023/Т10_DOMАndEvents_Exercise/Exercise/05. Fill-Dropdown/dropdown.js
+++ JS Front-End - 2023/Т10_DOMАndEvents_Exercise/Exercise/05. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    const textInput = document.querySelector('#newItemText');
+    const valueInput = document.querySelector('#newItemValue');
+
+    const option = document.createElement('option');
+    option.textContent = textInput.value;
+    option.value = valueInput.value;
+
+    const menu = document.querySelector('#menu');
+    menu.appendChild(option);
+
+    textInput.value = "";
+    valueInput.value = "";
 }
