5fd9e153bfa44a10f7ebaeee927cbed54efbb16a
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-07-23 18:28:25 +0300

    T11-P05

--- JS Front-End - 2023/T11_HTTPandREST/Resources/P05_GitHubReposByUsername/scripts/app.js
+++ JS Front-End - 2023/T11_HTTPandREST/Resources/P05_GitHubReposByUsername/scripts/app.js
@@ -1,3 +1,26 @@
 function loadRepos() {
-	console.log("TODO...");
+	const username = document.querySelector('#username').value;
+	const list = document.querySelector('#repos');
+
+	list.innerHTML = '';
+
+	fetch(`https://api.github.com/users/${username}/repos`)
+		.then((res) => res.json())
+		.then((repos) => {
+			repos.forEach((repo) => {
+				const item = document.createElement('li');
+				const adress = document.createElement('a');
+				adress.setAttribute("href", repo.html_url);
+				adress.textContent = `${repo.full_name}`;
+
+				item.appendChild(adress);
+				list.appendChild(item);
+			});
+		})
+		.catch((error) => {
+			const item = document.createElement('li');
+			item.textContent = `Error: ${error.status}`;
+
+			list.appendChild(item);
+		});	
 }
