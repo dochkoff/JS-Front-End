af2bd97ab8d4d9f74a6ba23312741eb4aeb6ba19
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-07-25 23:44:50 +0300

    T12-P01

--- JS Front-End - 2023/T12_HTTPandREST_Exercise/Exercise/01.Bus-Stop/app.js
+++ JS Front-End - 2023/T12_HTTPandREST_Exercise/Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,24 @@
 function getInfo() {
-    // TODO:
+    const busStopID = document.querySelector("#stopId").value;
+    const list = document.querySelector('ul');
+    const stopName = document.querySelector('#stopName');
+
+    list.innerHTML='';
+
+    fetch(`http://localhost:3030/jsonstore/bus/businfo/${busStopID}`)
+        .then((res) => res.json())
+        .then((busStop) => {
+            stopName.textContent = busStop.name;
+
+            Object.entries(busStop.buses).map(([busNumber, timeInMinutes]) => {
+                const item = document.createElement('li');
+                item.textContent = `Bus ${busNumber} arrives in ${timeInMinutes} minutes`;
+
+                list.appendChild(item);
+            });
+        })
+        .catch(() => {
+            stopName.textContent = 'Error';
+            return;
+        });
 }
