0c9152328fc83595b280b91260c8b5e5f25820ac
dochkoff <hounds.07cinder@icloud.com>
2023-07-26 18:48:50 +0300

    T12-P02

--- JS Front-End - 2023/T12_HTTPandREST_Exercise/Exercise/02.Bus-Schedule/app.js
+++ JS Front-End - 2023/T12_HTTPandREST_Exercise/Exercise/02.Bus-Schedule/app.js
@@ -1,10 +1,34 @@
 function solve() {
+    const departButton=document.querySelector('#depart');
+    const arriveButtno=document.querySelector('#arrive');
+    const busInfoBox=document.querySelector('#info');
+
+    let busStopInfo = {
+        name: '',
+        next: 'depot'
+    };
+
     function depart() {
-        // TODO: 
+        
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${busStopInfo.next}`)
+        .then((res)=>res.json())
+        .then((bustStopID)=>{
+            busStopInfo=bustStopID;
+            departButton.disabled=true;
+            arriveButtno.disabled=false;
+            busInfoBox.textContent=`Next stop ${busStopInfo.name}`;
+        })
+        .catch(()=>{
+            departButton.disabled=false;
+            arriveButtno.disabled=false;
+            busInfoBox.textContent=`Error`;
+        });
     }
 
     async function arrive() {
-        // TODO:
+        departButton.disabled=false;
+        arriveButtno.disabled=true;
+        busInfoBox.textContent=`Arriving at ${busStopInfo.name}`;
     }
 
     return {
