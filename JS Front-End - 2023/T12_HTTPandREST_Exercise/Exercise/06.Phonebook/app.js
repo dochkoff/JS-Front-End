e8dcf6050ebf46afa3de217cc9148faa9f52059a
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-07-30 22:53:14 +0300

    T12-P06

--- JS Front-End - 2023/T12_HTTPandREST_Exercise/Exercise/06.Phonebook/app.js
+++ JS Front-End - 2023/T12_HTTPandREST_Exercise/Exercise/06.Phonebook/app.js
@@ -1,5 +1,73 @@
 function attachEvents() {
-    // TODO:
+    const PHONEBOOK_URL = `http://localhost:3030/jsonstore/phonebook/`;
+    const phonebookContainer = document.querySelector('#phonebook');
+    const loadButton = document.querySelector('#btnLoad');
+    const createButton = document.querySelector('#btnCreate');
+    const personInput = document.querySelector('#person');
+    const phoneInput = document.querySelector('#phone');
+
+    loadButton.addEventListener('click', loadPhonebook);
+    createButton.addEventListener('click', createContactEntry);
+
+
+    function loadPhonebook() {
+        fetch(PHONEBOOK_URL)
+            .then((res) => res.json())
+            .then((phonebook) => {
+                const phonebookData = Object.values(phonebook);
+                phonebookContainer.innerHTML = '';
+
+                for (const { person, phone, _id } of phonebookData) {
+                    row = document.createElement('li');
+                    row.innerHTML = `${person}: ${phone}`;
+
+                    const deleteButton = document.createElement('button');
+                    deleteButton.setAttribute('id', _id);
+                    deleteButton.textContent = 'Delete';
+                    deleteButton.addEventListener('click', deleteContactEntry);
+
+                    row.appendChild(deleteButton);
+                    phonebookContainer.appendChild(row);
+                };
+            }
+        )
+    }
+
+    function deleteContactEntry() {
+        const key = this.id;
+
+        fetch(`${PHONEBOOK_URL}${key}`, {
+            method: 'DELETE'
+        })
+            .then((res) => res.json())
+            .then(() => loadPhonebook())
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+
+    function createContactEntry() {
+        person = personInput.value;
+        phone = phoneInput.value;
+
+        fetch(PHONEBOOK_URL, {
+            method: 'POST',
+            body: JSON.stringify({ person, phone })
+        })
+            .then((res) => res.json())
+            .then(() => {
+                loadPhonebook();
+                personInput.value = '';
+                phoneInput.value = '';
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+
+        
+    }
+
+
 }
 
 attachEvents();
